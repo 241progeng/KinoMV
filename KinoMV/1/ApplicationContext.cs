8fc46fb99a6a849f6bd8078d7e6861abbbadac07
metahet <metahet@ukr.net>
2018-11-08 18:46:53 +0200

    -

---
+++ KinoMV/1/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace _1
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext()
+            : base("DefaultConnection")
+        {
+        }
+        public DbSet<Profession> Professions { get; set; }
+    }
+}
---
+++ KinoMV/1/Profession.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace _1
+{
+    public class Profession : INotifyPropertyChanged
+    {
+        private int prof_id;
+        private string prof_name;
+        private int mult_id;
+
+        public int Prof_ID
+        {
+            get
+            { return prof_id; }
+
+            set
+            {
+                prof_id = value;
+                OnPropertyChanged("Profession ID");
+            }
+        }
+
+        public string Prof_Name
+        {
+            get
+            { return prof_name; }
+
+            set
+            {
+                prof_name = value;
+                OnPropertyChanged("Profession Name");
+            }
+        }
+
+        public int Mult_ID
+        {
+            get
+            { return mult_id; }
+
+            set
+            {
+                mult_id = value;
+                OnPropertyChanged("Multiple Occupation");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
